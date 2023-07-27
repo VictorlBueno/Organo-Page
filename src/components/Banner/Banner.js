9133cc49b8efe53dafabe48286485315e6df3841
Victor <vihbuenoz@gmail.com>
2023-07-27 19:24:20 -0300

    added banner and textfield components

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+// JSX
+export const Banner = () => {
+    return (
+
+        <header className="banner">
+            <img src="/images/banner.png" alt="Banner príncipal" />
+        </header>
+
+    )
+}
