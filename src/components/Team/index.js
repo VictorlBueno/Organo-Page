a55bf8edc66c18b16bf91166d93e25a6f975d3cc
Victor <vihbuenoz@gmail.com>
2023-08-01 18:38:52 -0300

    added workers card

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,11 +1,30 @@
 import "./Team.css";
+import Worker from "../Worker";
 
 const Team = (props) => {
     return (
-        <section className="team">
-            <h3>{props.name}</h3>
-        </section>
-    )
+        props.workers.length > 0 && (
+            <section
+                className="team"
+                style={{ backgroundColor: props.secondaryColor }}
+            >
+
+                <h3 style={{ borderColor: props.primaryColor }}>
+                    {props.name}
+                </h3>
+
+                <div className="workers">
+                    {props.workers.map((worker) => (
+                        <Worker
+                            name={worker.name}
+                            position={worker.position}
+                            image={worker.image}
+                        />
+                    ))}
+                </div>
+            </section>
+        )
+    );
 };
 
 export default Team;
